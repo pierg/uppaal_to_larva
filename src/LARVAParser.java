03a570cb2ac10ea2c29be0ca21e8b94f3e7e024f
PoissonSoluble <vincent.duplessis14@gmail.com>
2017-07-03 18:10:00 +0200

    generate larva file with several properties

--- src/LARVAParser.java
+++ src/LARVAParser.java
@@ -31,195 +31,124 @@ import org.xml.sax.SAXException;
  * @author Brissy Maxence & Duplessis Vincent
  */
 public class LARVAParser {
-     
+
     private Document getXMLDocument(String filename) throws ParserConfigurationException, SAXException, IOException {
         File xmlFile = new File(filename);
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         InputSource inputSource = new InputSource(new StringReader(readFile(xmlFile)));
-        
+
         return builder.parse(inputSource);
     }
-    
+
     /**
-     * Read an XML code from a text file and delete the DOCTYPE tag because of bugs 
+     * Read an XML code from a text file and delete the DOCTYPE tag because of
+     * bugs
+     *
      * @param inputFile the file we want to extract the XML code from
      * @return the XML string without the DOCTYPE tag
      */
-    private String readFile(File inputFile){
+    private String readFile(File inputFile) {
         String outputString = "";
-        
+
         try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
 
             String sCurrentLine;
 
             while ((sCurrentLine = br.readLine()) != null) {
-                outputString += sCurrentLine+"\n";
+                outputString += sCurrentLine + "\n";
             }
         } catch (IOException e) {
-            System.err.println("Exception in readFile method : "+e);
+            System.err.println("Exception in readFile method : " + e);
         }
-        
+
         outputString = outputString.replaceFirst("<!DOCTYPE nta PUBLIC '-//Uppaal Team//DTD Flat System 1.1//EN' 'http://www.it.uu.se/research/group/darts/uppaal/flat-1_2.dtd'>", "");
-        
-        return outputString; 
+
+        return outputString;
     }
-    
+
     /*
-	 * Get the content of the template file
-	 */
+     * Get the content of the template file
+     */
     private String getTemplate(String templateName) {
         BufferedReader reader;
         StringBuilder stringBuilder = new StringBuilder();
         try {
             reader = new BufferedReader(new FileReader(templateName));
-            String line = null;
+            String line;
             String ls = System.getProperty("line.separator");
 
             while ((line = reader.readLine()) != null) {
                 stringBuilder.append(line);
-		stringBuilder.append(ls);
+                stringBuilder.append(ls);
             }
             reader.close();
         } catch (IOException e) {
             System.err.println("Cannot find the JAVA template.");
-	}
+        }
         return stringBuilder.toString();
     }
-    
-    private List<State> getStates(Document document) {
-	Element root = document.getDocumentElement();
-	NodeList locations = root.getElementsByTagName("location");
 
-	List<State> states = new ArrayList<>();
-
-	for (int i = 0; i < locations.getLength(); i++) {
-            Element location = (Element) (locations.item(i));
-            String state = location.getTextContent();
-            states.add(new State(location.getAttribute("id"), state));
-	}
-
-	return states;
-    }
-    
-    private List<Transition> getTransitions(Document document, List<State> states) {
-	Element root = document.getDocumentElement();
-	NodeList transitionNodes = root.getElementsByTagName("transition");
-
-	List<Transition> transitions = new ArrayList<>();
-
-	for (int i = 0; i < transitionNodes.getLength(); i++) {
-            Element transitionNode = (Element) (transitionNodes.item(i));
-            String idFrom = ((Element) (transitionNode.getElementsByTagName("source").item(0))).getAttribute("ref");
-            String idTo = ((Element) (transitionNode.getElementsByTagName("target").item(0))).getAttribute("ref");
-            NodeList labels = transitionNode.getElementsByTagName("label");
-            transitions.add(getTransitionFromNode(states, i, idFrom, idTo, labels));
-        }
-
-	return transitions;
-    }
-
-    private Transition getTransitionFromNode(List<State> states, int i, String idFrom, String idTo, NodeList labels) {
-	State from = new State();
-	State to = new State();
-	String guard = new String();
-	String assignment = new String();
-	for (int j = 0; j < labels.getLength(); j++) {
-            Element label = (Element) (labels.item(j));
-            if (label.getAttribute("kind").equals("guard")) {
-		guard = label.getTextContent();
-            } else if (label.getAttribute("kind").equals("assignment")) {
-                assignment = label.getTextContent();
-            }
-        }
-
-	for (State state : states) {
-            if (state._id.equals(idFrom)) {
-                from = state;
-            }
-
-            if (state._id.equals(idTo)) {
-		to = state;
-		}
-	}
-	return new Transition(from, to, guard, assignment);
-    }
-    
-    private String getStateDeclaration(List<State> states) {
-	String stateDeclaration = "";
-	for (State state : states) {
-		if (!state._name.equals("start")) {
-        		stateDeclaration += state + " ";
-		}
-	}
-
-        return stateDeclaration;
-    }        
-    
-
-    private String getResetTransitions(List<State> states) {
-	String statement = "->start[reset()\\EchoServer.reward = 0;EchoServer.response();]\n";
-	String resetTransition = "start" + statement;
-
-        for (State state : states) {
-            resetTransition += "\t\t\t" + state + statement;
-	}
-
-	return resetTransition;
-    }
-    
-    private String getTransitionsCode(List<Transition> transitions) {
-	String transitionsCode = "";
-	for (Transition tr : transitions) {
-		transitionsCode += "\t\t\t" + tr._from + "->" + tr._to;
-			
-		if(!tr._guard.equals("") && !tr._guard.equals("none")){
-			transitionsCode += "[rlevent\\EchoServer." + tr._guard;
-		}else{
-                    transitionsCode += "[rlevent\\";
-		}
-			
-		if(!tr._assignment.equals("") && !tr._assignment.equals("none")){
-                    transitionsCode += "\\EchoServer." + tr._assignment + ";EchoServer.response();]\n";
-		}else{  
-                    transitionsCode += "\\EchoServer.response();]\n";
-		}
-			
-	}
-
-        return transitionsCode;
-    }
-    
     private String mergeCodes(String template, String upaallCode, String replacedPattern) {
-	return template.replaceAll(replacedPattern, upaallCode);
+        return template.replaceAll(replacedPattern, upaallCode);
     }
-    
-    private String getLarvaDocument(Document document) {
-	String larvaTemplate = getTemplate("larva_template");
 
-	List<State> states = getStates(document);
-	List<Transition> transitions = getTransitions(document, states);
+    private List<UppaalTemplate> getUppaalTemplates(Document document) {
+        List<UppaalTemplate> uppaalTemplates = new ArrayList<>();
 
-	String stateDeclaration = getStateDeclaration(states);
+        Element root = document.getDocumentElement();
+        NodeList templates = root.getElementsByTagName("template");
 
-	String transitionsCode = getResetTransitions(states) + getTransitionsCode(transitions);
+        for (int i = 0; i < templates.getLength(); i++) {
+            Element template = (Element) templates.item(i);
 
-	String larvaDocument = mergeCodes(larvaTemplate, stateDeclaration, "<<<UPPAAL_STATES>>>");
-	larvaDocument = mergeCodes(larvaDocument, transitionsCode, "<<<UPPAAL_TRANSITIONS>>>");
-
-        return larvaDocument;
-    }
-    
-    public void parseFile(String filename,String destination) throws IOException, ParserConfigurationException, SAXException {
-	Document document = getXMLDocument(filename);
-
-	String larvaDocument = getLarvaDocument(document);
-		
-        List<String> larvaList = Arrays.asList(larvaDocument.split("\\n"));
-	
-        Path larvaPath = Paths.get(destination+".lrv");
+            Element name = (Element) (template.getElementsByTagName("name").item(0));
+            
+            if(name.getTextContent().matches("Sim_(.*)")){
+                continue;
+            }
+            
+            UppaalTemplate uppaalTemplate = new UppaalTemplate(name.getTextContent());
+            uppaalTemplate.retrieveStates(template);
+            uppaalTemplate.retrieveTransitions(template);
+            uppaalTemplates.add(uppaalTemplate);
+        }
         
+        return uppaalTemplates;
+    }
+
+    private String getLarvaDocument(Document document) {
+        String larvaTemplate = getTemplate("larva_template");
+        String larvaPropertyTemplate = getTemplate("larva_property_template");
+        String larvaProperties = new String();
+        
+        List<UppaalTemplate> uppaalTemplates = getUppaalTemplates(document);
+
+        larvaProperties = uppaalTemplates.stream().map((UppaalTemplate uppaalTemplate) -> {
+            String stateDeclaration = uppaalTemplate.getStateDeclaration();
+            String transitionsCode = uppaalTemplate.getResetTransitions() + uppaalTemplate.getTransitionsCode() + uppaalTemplate.getDefaultTransitions();
+            String larvaProperty;
+            larvaProperty = mergeCodes(larvaPropertyTemplate, uppaalTemplate.getProperty(), "<<<UPPAAL_PROPERTY>>>");
+            larvaProperty = mergeCodes(larvaProperty, stateDeclaration, "<<<UPPAAL_STATES>>>");
+            larvaProperty = mergeCodes(larvaProperty, transitionsCode, "<<<UPPAAL_TRANSITIONS>>>");
+
+            return larvaProperty;
+        }).map((larvaProperty) -> larvaProperty).reduce(larvaProperties, String::concat);
+
+
+        return mergeCodes(larvaTemplate, larvaProperties, "<<<UPPAAL_PROPERTIES>>>");
+    }
+
+    public void parseFile(String filename, String destination) throws IOException, ParserConfigurationException, SAXException {
+        Document document = getXMLDocument(filename);
+
+        String larvaDocument = getLarvaDocument(document);
+
+        List<String> larvaList = Arrays.asList(larvaDocument.split("\\n"));
+
+        Path larvaPath = Paths.get(destination + ".lrv");
+
         Files.write(larvaPath, larvaList, Charset.forName("UTF-8"));
     }
 }
---
+++ src/UppaalTemplate.java
@@ -0,0 +1,137 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author chalmers-internship
+ */
+public class UppaalTemplate {
+
+    private final String _property;
+    private final List<State> _states;
+    private final List<Transition> _transitions;
+
+    public UppaalTemplate(String property) {
+        _property = property;
+        _states = new ArrayList<>();
+        _transitions = new ArrayList<>();
+    }
+
+    public void addState(State state) {
+        _states.add(state);
+    }
+
+    public void addTransition(Transition transition) {
+        _transitions.add(transition);
+    }
+
+    public void retrieveStates(Element template) {
+        NodeList locations = template.getElementsByTagName("location");
+
+        for (int i = 0; i < locations.getLength(); i++) {
+            Element location = (Element) (locations.item(i));
+            String state = location.getTextContent();
+            _states.add(new State(location.getAttribute("id"), state.trim()));
+        }
+        
+        _states.add(new State("0", "default"));
+    }
+
+    public void retrieveTransitions(Element template) {
+        NodeList transitionNodes = template.getElementsByTagName("transition");
+
+        for (int i = 0; i < transitionNodes.getLength(); i++) {
+            Element transitionNode = (Element) (transitionNodes.item(i));
+            String idFrom = ((Element) (transitionNode.getElementsByTagName("source").item(0))).getAttribute("ref");
+            String idTo = ((Element) (transitionNode.getElementsByTagName("target").item(0))).getAttribute("ref");
+            NodeList labels = transitionNode.getElementsByTagName("label");
+            _transitions.add(getTransitionFromNode(_states, i, idFrom, idTo, labels));
+        }
+    }
+
+    public String getStateDeclaration() {
+        String stateDeclaration = "";
+        stateDeclaration = _states.stream().filter((state) -> (!state._name.equals("start"))).map((state) -> state + " ").reduce(stateDeclaration, String::concat);
+
+        return stateDeclaration;
+    }
+
+    public String getTransitionsCode() {
+        String transitionsCode = "";
+        for (Transition tr : _transitions) {
+            transitionsCode += "\t\t\t" + tr._from + "->" + tr._to;
+
+            if (!tr._guard.equals("") && !tr._guard.equals("none")) {
+                transitionsCode += "[rlevent\\\\\\\\\\EchoServer." + tr._guard;
+            } else {
+                transitionsCode += "[rlevent\\\\\\\\\\";
+            }
+
+            if (!tr._assignment.equals("") && !tr._assignment.equals("none")) {
+                transitionsCode += "\\\\\\\\\\EchoServer." + tr._assignment + ";EchoServer.propertyChecked();]\n";
+            } else {
+                transitionsCode += "\\\\\\\\\\EchoServer.propertyChecked();]\n";
+            }
+
+        }
+
+        return transitionsCode;
+    }
+
+    public String getResetTransitions() {
+        String statement = "->start[reset()\\\\\\\\\\EchoServer.reward = 0;EchoServer.propertyChecked();]\n";
+        String resetTransition = "start" + statement;
+
+        resetTransition = _states.stream().map((state) -> "\t\t\t" + state + statement).reduce(resetTransition, String::concat);
+        
+        return resetTransition;
+    }
+    
+    public String getDefaultTransitions() {
+        String statement = "->default[rlevent()\\\\\\\\\\\\\\\\\\\\EchoServer.reward = 0;EchoServer.resetAgent();]\n";
+        String defaultTransition = "\t\t\tstart" + statement;
+
+        defaultTransition = _states.stream().map((state) -> "\t\t\t" + state + statement).reduce(defaultTransition, String::concat);
+        
+        return defaultTransition;
+    }
+
+    public String getProperty() {
+        return _property;
+    }
+
+    private Transition getTransitionFromNode(List<State> states, int i, String idFrom, String idTo, NodeList labels) {
+        State from = new State();
+        State to = new State();
+        String guard = new String();
+        String assignment = new String();
+        for (int j = 0; j < labels.getLength(); j++) {
+            Element label = (Element) (labels.item(j));
+            if (label.getAttribute("kind").equals("guard")) {
+                guard = label.getTextContent();
+            } else if (label.getAttribute("kind").equals("assignment")) {
+                assignment = label.getTextContent();
+            }
+        }
+
+        for (State state : states) {
+            if (state._id.equals(idFrom)) {
+                from = state;
+            }
+
+            if (state._id.equals(idTo)) {
+                to = state;
+            }
+        }
+        return new Transition(from, to, guard, assignment);
+    }
+
+}
